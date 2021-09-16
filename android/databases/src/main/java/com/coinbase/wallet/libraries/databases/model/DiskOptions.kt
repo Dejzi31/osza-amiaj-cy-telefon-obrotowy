5a9d2ebf99b9ab5314113371fe272a8401580c72
Zaneta <60270126+Dejzi31@users.noreply.github.com>
2021-09-16 21:15:25 +0200

    Initialize spencerstock-add-readme

---
+++ android/databases/src/main/java/com/coinbase/wallet/libraries/databases/model/DiskOptions.kt
@@ -0,0 +1,22 @@
+package com.coinbase.wallet.libraries.databases.model
+
+import android.content.Context
+import androidx.room.migration.Migration
+import com.coinbase.wallet.libraries.databases.interfaces.StorageOptions
+
+/**
+ * Options for Room sqlite database
+ *
+ * @property context Android context used to create room db
+ * @property providerClazz Room-specific database provider
+ * @property dbName Name of the database
+ * @property migrations list of migrations to include
+ * @property destructiveFallback If true and a migration is missing, the database will be whipped
+ */
+data class DiskOptions<T>(
+    val context: Context,
+    val providerClazz: Class<T>,
+    val dbName: String,
+    val migrations: List<Migration> = emptyList(),
+    val destructiveFallback: Boolean = false
+) : StorageOptions
